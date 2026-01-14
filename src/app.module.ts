a3734a85b69ac24495f24e35d873d6d13af38529
KaushikShubham123 <shubhamsharma006jan@gmail.com>
2026-01-14 10:32:24 +0530

    feat:added rate limiting ,config for database

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,20 +5,39 @@ import { JobsModule } from './jobs/jobs.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Job } from './jobs/entities/job.entity';
 import { ScheduleModule } from '@nestjs/schedule';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { ThrottlerModule, ThrottlerGuard } from '@nestjs/throttler';
+import { APP_GUARD } from '@nestjs/core';
 
 @Module({
-  imports: [TypeOrmModule.forRoot({
-    type: 'postgres',
-    host: 'localhost',
-    port: 5432,
-    username: 'postgres',
-    database: 'media_queue',
-    entities: [Job],
-    synchronize: true,
+  imports: [ConfigModule.forRoot({
+    isGlobal: true,
+  }),
+  TypeOrmModule.forRootAsync({
+    inject: [ConfigService],
+    useFactory: (config: ConfigService) => ({
+      type: 'postgres',
+      host: config.get('DB_HOST'),
+      port: Number(config.get('DB_PORT')),
+      username: config.get('DB_USER'),
+
+      database: config.get('DB_NAME'),
+      autoLoadEntities: true,
+      synchronize: true,
+    })
+  }),
+  ThrottlerModule.forRoot({
+    throttlers: [{
+      ttl: 60000,
+      limit: 10,
+    }]
   }),
   ScheduleModule.forRoot(),
     JobsModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, {
+    provide: APP_GUARD,
+    useClass: ThrottlerGuard
+  }],
 })
 export class AppModule { }
